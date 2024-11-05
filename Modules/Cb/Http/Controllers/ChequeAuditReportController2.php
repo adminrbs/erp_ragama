c0a2552011b9ef462daefeed531630cb438fb65f
Samitha Lowe <58819808+Samitharu@users.noreply.github.com>
2024-11-05 16:23:22 +0530

    05/11

--- Modules/Cb/Http/Controllers/ChequeAuditReportController2.php
+++ Modules/Cb/Http/Controllers/ChequeAuditReportController2.php
@@ -64,7 +64,7 @@ class ChequeAuditReportController2 extends Controller
         
         CAST(DATEDIFF(customer_receipts.receipt_date,debtors_ledgers.trans_date)AS SIGNED) AS Age,
         (SELECT debtors_ledgers.amount  FROM debtors_ledgers WHERE customer_receipt_setoff_data.reference_external_number = debtors_ledgers.external_number LIMIT 1) AS  Invoice_amont,
-        (SELECT customer_receipt_setoff_data.paid_amount  FROM customer_receipt_setoff_data WHERE customer_receipt_setoff_data.reference_external_number = debtors_ledgers.external_number GROUP BY debtors_ledgers.external_number),
+        (SELECT customer_receipt_setoff_data.paid_amount  FROM customer_receipt_setoff_data WHERE customer_receipt_setoff_data.reference_external_number = debtors_ledgers.external_number) AS total_paid,
         (SELECT SUM(sales_return_debtor_setoffs.setoff_amount) FROM sales_return_debtor_setoffs WHERE sales_return_debtor_setoffs.external_number = debtors_ledgers.external_number),
         customer_receipt_setoff_data.set_off_amount,
         ((SELECT debtors_ledgers.amount  FROM debtors_ledgers WHERE customer_receipt_setoff_data.reference_external_number = debtors_ledgers.external_number LIMIT 1) - (SELECT debtors_ledgers.paidamount  FROM debtors_ledgers WHERE customer_receipt_setoff_data.reference_external_number = debtors_ledgers.external_number LIMIT 1)) AS balance,
@@ -79,7 +79,7 @@ class ChequeAuditReportController2 extends Controller
         LEFT JOIN customers ON customer_receipts.customer_id = customers.customer_id
         LEFT JOIN banks ON customer_receipt_cheques.bank_id = banks.bank_id
         LEFT JOIN bank_branches ON customer_receipt_cheques.bank_branch_id = bank_branches.bank_branch_id' . $query_modify;
-            //dd($qry);
+            dd($qry);
             $result = DB::select($qry);
             //dd($result);
             $resulcustomer = DB::select('select customer_receipts.customer_receipt_id,customers.customer_name,CRC.cheque_number 
